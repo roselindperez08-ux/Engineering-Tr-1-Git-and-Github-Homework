4b09f279d3d3dc75072632f856c5923bddb35332
Rose Perez <roselindperez08@gmail.com>
2026-07-26 10:01:21 -0700

    Fix Yosemite Website

---
+++ Engineering-Tr-1-Git-and-Github-Homework-main/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Yosemite National Park</h1>
+
+      <img
+        src="scenery.jpeg"
+        alt="Half Dome"
+        className="yosemite-image"
+      />
+
+      <p>
+        A picture of Yosemite National Park&apos;s Half Dome, halfway through the 4 Mile Trail.
+      </p>
+    </div>
+  );
+}
+
+export default App;
